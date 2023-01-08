035acaad963c55d8298ad857a679c15e7d88990b
iuzairsheikh <uzairsheikh595@gmail.com>
2023-01-08 20:43:09 +0500

    update

---
+++ app.js
@@ -0,0 +1,143 @@
+// alert("Good Morning");
+// alert("Error!Enter a valid Password");
+// alert("Welcome to JS land...\nHappy coding");
+// alert("Welcome t JS land..");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// var  username
+// var  myName ="Uzairsheikh"
+// alert(myName);
+
+// var message
+// var message = "Hello wolrd"
+// alert(message);
+
+// var studentName = "Jhone Date"
+// alert(studentName);
+
+// var studentage = "15 years"
+// alert(studentage);
+
+// var studentBio = "Certified Mobile Appilication Development"
+// alert(studentBio);
+
+// var kkkk = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(kkkk);
+
+// document.write("Yeh! I can write HTML content through JavaScript")
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a);
+
+// var a = "My Email address is example@gmail.com"
+// alert(a);
+
+
+
+// var username = "Uzair Sheikh" 
+// var saas = prompt("enter your name")
+// if(username === saas){
+//     document.write("Welcome my HTML page")
+// }
+// else{
+//     document.write("Check your Username")
+// }
+
+// var book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+// alert(book);
+
+// var age =" i am 15 years old"
+// alert(age)
+
+// var birthYear = 1990
+// document.write("My birth year is 1990<br>Data type of my ddeclared variable is number ")
+
+// var a =5
+// var b = 10 
+// var c = a+b;
+// alert(c)
+
+// document.write("<h1>Rules for naming JS variable</h1><br>Variable names can only contain numbers, $,and _. For example $my_1stVariable<br> Variables must begin with a Letters, $ or _. For example $name, _name or name<br>Variable names are case sensitive <br>Variable names should not be JS Keywords ")
+
+// var a = 3
+// var b = 5
+// var c = a+b
+// alert(c)
+// document.write("Sum of 3 amd 5 is 8")
+
+//  var a = 3
+//  var b = 5
+//  var c = a-b
+//  alert(c)
+//  document.write("Sum of 3 amd 5 substract is -2")
+
+//  var a = 3
+//  var b = 5
+//  var c = a*b
+//  alert(c)
+//  document.write("Sum of 3 amd 5 multiplay is 15");
+
+//  var a = 3
+//  var b = 5
+//  var c = a % b
+//  alert(c)
+//  document.write("Sum of 3 amd 5 Modules is 15");
+
+// var a = 600
+// var b = 5
+// var c = a*b
+// document.write("<b>Total cost to buy 5 tickets to a movie is 3000PKR</b>") 
+
+// const a = 10
+// const b = 1
+// const c = 4
+
+// para = document.getElementById('abc')
+
+// for(let i = 0; i < a.lenght; i++) {
+//    para.innerHTML += ``
+// }
+
+// document.write('1 + 4')
+
+// let date = new Date();
+// console.log(date);
+
+// const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// let date = new Date();
+// let gettingMonthFromArray = month[date.getMonth()];
+// alert(`Current month: ${gettingMonthFromArray}`)
+
+// const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+// let date = new Date();
+// let gettingDaysFromArrays = days[date.getDay()];
+// alert(`Today is ${gettingDaysFromArrays}`)
+
+// const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+// let date = new Date();
+// let gettingDaysFromArrays = days[date.getDay()];
+// if (gettingDaysFromArrays == "sunday" || gettingDaysFromArrays == "saturday" ) {
+//     alert(`Today is Fun Day!`)
+// } else {
+//     alert(`Today is working Day`)
+// };
+
+// let date = new Date();
+// let Days = date.getDate();
+// if(Days <= 16) {
+//     alert(`First fifteen days of the month`)
+// } else {
+//     alert(`Last days of the month`)
+// }
+
+// let gettingDate = new Date();
+// document.write(`<h2> ${gettingDate} </h2>`);
+// let gettingTime = gettingDate.getTime();
+// document.write(`<h2> Elapsed since January 1, 1970: ${gettingTime} </h2>`);
+// let gettingOldDate = new Date("01-01-2001");
+// console.log(gettingOldDate);
+// let set = gettingOldDate.setDate(1)
+// let set1 = gettingOldDate.setTime(12)
+// console.log(set);
+// let gettingTimes = gettingOldDate.getMinutes();
+// document.write(`<h2> Elapsed since January 1, 1970: ${gettingTimes} </h2>`);
